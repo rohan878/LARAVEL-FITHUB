cde92e3083ce8c9d117d232fab94b613b83b2b11
rohan878 <71791439+rohan878@users.noreply.github.com>
2025-04-21 06:15:08 +0600

    Add files via upload

---
+++ cse471_fithub.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 21, 2025 at 02:07 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cse471_fithub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+-- Error reading structure for table cse471_fithub.cache: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`cache`
+-- Error reading data for table cse471_fithub.cache: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`cache`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+-- Error reading structure for table cse471_fithub.cache_locks: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`cache_locks`
+-- Error reading data for table cse471_fithub.cache_locks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`cache_locks`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table cse471_fithub.failed_jobs: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`failed_jobs`
+-- Error reading data for table cse471_fithub.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`failed_jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table cse471_fithub.jobs: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`jobs`
+-- Error reading data for table cse471_fithub.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+-- Error reading structure for table cse471_fithub.job_batches: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`job_batches`
+-- Error reading data for table cse471_fithub.job_batches: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`job_batches`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table cse471_fithub.migrations: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`migrations`
+-- Error reading data for table cse471_fithub.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table cse471_fithub.password_reset_tokens: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`password_reset_tokens`
+-- Error reading data for table cse471_fithub.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`password_reset_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+-- Error reading structure for table cse471_fithub.sessions: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`sessions`
+-- Error reading data for table cse471_fithub.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`sessions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+-- Error reading structure for table cse471_fithub.tickets: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`tickets`
+-- Error reading data for table cse471_fithub.tickets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`tickets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table cse471_fithub.users: #1142 - SHOW command denied to user ''@'localhost' for table `cse471_fithub`.`users`
+-- Error reading data for table cse471_fithub.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `cse471_fithub`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
